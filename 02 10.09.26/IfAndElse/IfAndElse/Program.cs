2f32e45541b2ed9fd1fd922605b2bf33b56dbec1
lembitkogerma-source <lembit.kogerma@gmail.com>
2026-09-10 09:49:34 +0300

    Tere

---
+++ 02 10.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace IfAndElse
+{
+    //Projekt nimetusega IfAndElse, mille sees asub class nimega programm.
+    //See klsdd sisaldab Main meetodit, mis on programmi sisenemispunkt
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            //muutuja nimega nime, kuhu salvestatakse ksutaja sisestatud tekst
+            string name = Console.ReadLine();
+
+            // ! tähendab "ei ole" ja == tähendab "on võrdne"
+            if (name != "")
+            {
+                //KUi kasutaja sisestab midagi, siis muudetakse taustavärv
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("sisestasid enda nime");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sisestasid tühja nime");
+                // Kui kasutaja ei sisesta midagi, siis kostub 2 piiksu
+                Console.Beep();
+                // Ja oodatakse 1 sekund enne programmi lõppu
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
