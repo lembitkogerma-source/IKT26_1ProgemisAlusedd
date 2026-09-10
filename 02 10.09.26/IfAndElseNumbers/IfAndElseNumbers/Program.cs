5adb6549231a0eaeb3313044c17bc6db2a4bdb2c
lembitkogerma-source <lembit.kogerma@gmail.com>
2026-09-10 10:51:54 +0300

    IfAndElseNumbers

---
+++ 02 10.09.26/IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta number");
+            string name = Console.ReadLine();
+
+            if (name != "")
+            {
+              
+            }
+            else
+            {
+
+            }
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            Console.WriteLine("sisestasid enda numbri");
+            Console.WriteLine(name);
+
+            
+
+        }
+    }         
+}
+          
