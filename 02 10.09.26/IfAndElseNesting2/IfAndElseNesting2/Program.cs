837fd509d856df33239fc7f8df42142cc0d90e46
lembitkogerma-source <lembit.kogerma@gmail.com>
2026-09-10 11:41:56 +0300

    IfAndElseNesting2

---
+++ 02 10.09.26/IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If and else nesting");
+
+            int y = 10;
+
+            if (y == 12
+           } }
+    
+    
+ )
+        }
+    }
+}
