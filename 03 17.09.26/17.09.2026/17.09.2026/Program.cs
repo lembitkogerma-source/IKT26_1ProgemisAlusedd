a5aa403248eff51e21d672c15be0e89b9389f1b6
lembitkogerma-source <lembit.kogerma@gmail.com>
2026-09-17 10:49:22 +0300

    IfAndElseAge

---
+++ 03 17.09.26/17.09.2026/17.09.2026/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _17._09._2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teha üks if and else nestimine iseseisvalt");
+            Console.WriteLine("teise else if-i sisse panna if and else nestimine");
+            //konsool küsib numbrit
+            //if võrdub 12
+            //else if ja siia sisse omakorda teha if ning else. Else if tingimus on, et
+            //muutuja on suurem kui 20
+            //else ja seal väljastab konsool teksti: mingid kahtlased väärtused
+
+            string number = Console.ReadLine();
+            int numberInt = int.Parse(number);
+
+            if (numberInt == 12)
+            {
+                Console.WriteLine("number on 12");
+            }
+            else if (numberInt > 20)
+            {
+                if (numberInt > 30)
+                {
+                    Console.WriteLine("sinu vanus on 31 või vanem");
+                }
+                else
+                {
+                    Console.WriteLine("sinu vanus on 21 kuni 30");
+                }                   
+            }
+            else
+            {
+                Console.WriteLine("kahtlane väärtus");
+            }
+        }     
+    }
+}
